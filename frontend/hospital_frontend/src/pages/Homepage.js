4e087e127023e36def838c61c99e0347d9e67a23
Eda Ecesu <ecesu.cinar@sabanciuniv.edu>
2025-08-08 22:37:42 +0200

    Find a new show and really wnated to watch that but I needed to keep my github streak goign so here we are
    I am not sure about this at all but a progress does make me feel better. I hope I can design a better ui in the future but right now my imagination is very limited. Which saddens me and reminds me why I gave up on being an artist and got into cs in the first place I really wanted to be good at art and still do :)

--- frontend/hospital_frontend/src/pages/Homepage.js
+++ frontend/hospital_frontend/src/pages/Homepage.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import Foto from '../assets/akad.jpg'
 
 const Homepage = () => {
     return(
         <div>
+            <div 
+                className='bg-cover bg-center bg-no-repeat h-[85vh] bg-slate-700 bg-blend-overlay flex flex-col justify-center item-center text-center'
+                style={{ backgroundImage: `url(${Foto})` }}
+            >
+                
+                <h1 
+                    className='text-white md:text-6xl text-4xl font-bold px-4 md:pb-6 pb-3'
+                >
+                    Özel Akademi Cerrahi Tıp Merkezi
+                </h1>
+
+                <p
+                    className='text-white md:text-2xl text-lg font-normal md:p-4 p-7 pb-3'
+                >
+                    Olmaya devlet cihanda bir nefes sıhhat gibi!
+                </p>
+
+                <a
+                    href ="/Contact"
+                    className='text-white inline-block border border-white rounded-full m-10 md:mx-auto px-8 py-1.5 text-base bg-transparent relative cursor-pointer transition-all duration-100 hover:bg-opacity-15 hover:bg-white'
+                >
+                     Daha fazla bilgi için bize ulaşın 
+                </a>
+
+            </div>
 
         </div>
     );
