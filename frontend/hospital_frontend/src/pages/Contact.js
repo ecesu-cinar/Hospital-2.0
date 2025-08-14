8af62ffd0742bde9cc5c800da5ba96dcb6d8bc1f
Eda Ecesu <ecesu.cinar@sabanciuniv.edu>
2025-08-14 22:14:09 +0200

    ContactUs bitti gibi yipiie
    My only problem is that the icons in the mobile are not listed as I wanted them to be and kind bugged me maybe I'll change that sometime don't know it yet tho. I just want to finish the project at this point and start something related to ai.

--- frontend/hospital_frontend/src/pages/Contact.js
+++ frontend/hospital_frontend/src/pages/Contact.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from '../components/Header'
 import HeaderPhoto from '../assets/photos/Contact.jpg'
+import { FaLocationDot, FaXTwitter } from "react-icons/fa6";
+import { FaPhoneAlt , FaInstagram, FaFacebookF } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
 
 const Contact = () => {
     return(
@@ -21,15 +24,142 @@ const Contact = () => {
                         title="Akademi Cerrahi Tıp Merkezi Haritası"
                     ></iframe>
                 </div>
-
-                <div
-                    className=''
-                >
-                    
-                </div>
                 
-            </div>
+                <div
+                    className='flex flex-col justify-center mx-1 md:mx-20'
+                >
+                    <div
+                        className='grid grid-cols-1 md:grid-cols-2 gap-y-12 gap-x-3 md:gap-y-16 md:gap-x-52 mx-auto text-primary mb-32 mt-12 items-center'
+                    >
+                        <div className="text-left">
+                            <a
+                                href="https://maps.app.goo.gl/agU5UYcjWcjBxrk39"
+                                className="flex items-center gap-4 hover:text-secondary group transition duration-300"
+                            >
+                                <FaLocationDot className="text-3xl md:text-4xl shrink-0" />
 
+                                <div>
+                                <h3 className="text-base md:text-xl font-medium select-text">
+                                    Fatih Mahallesi Velioğlu Caddesi No: 74
+                                </h3>
+
+                                <p className="text-sm md:text-base text-primary/70 group-hover:text-secondary transition duration-300">
+                                    Bağcılar / İSTANBUL
+                                </p>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div
+                            className='text-left'
+                        >
+                            <a href="https://www.instagram.com/ozelakademitipmerkezi/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className='flex items-center gap-4 hover:text-secondary transition duration-300'
+                            >
+                                
+                                <FaInstagram 
+                                    className='text-3xl md:text-4xl shrink-0'
+                                />
+                                
+                                <h3
+                                    className='text-base md:text-xl font-medium select-text'
+                                >
+                                    @ozelakademitipmerkezi
+                                </h3>
+                            
+                            </a>
+                        </div>
+
+                        <div className='text-left'>
+                            <a
+                                href="tel:+902124740266"
+                                className='flex items-center gap-4 hover:text-secondary transition duration-300'    
+                            >
+
+                                <FaPhoneAlt 
+                                    className='text-3xl md:text-4xl shrink-0'
+                                />
+
+                                <h3
+                                    className='text-base md:text-xl font-medium select-text'
+                                >
+                                    (0 212) 474 02 66
+                                </h3>
+
+                            </a>
+
+                        </div>
+
+                        <div
+                            className='text-left'
+                        >
+                            <a href="https://www.facebook.com/ozelakademitipmerkezi/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className='flex items-center gap-4 hover:text-secondary transition duration-300'
+                            >
+                                
+                                <FaFacebookF
+                                    className='text-3xl md:text-4xl shrink-0'
+                                />
+                                
+                                <h3
+                                    className='text-base md:text-xl font-medium select-text'
+                                >
+                                    Akademi Tıp
+                                </h3>
+                            
+                            </a>
+                        </div>
+
+                        <div
+                            className='text-left'
+                        >
+                            <a 
+                                href="mailto:akademitipmerkezi@windowslive.com"
+                                className='flex items-center gap-4 hover:text-secondary transition duration-300'    
+                            >
+                                
+                                <HiMail 
+                                    className='text-3xl md:text-4xl shrink-0'
+                                />
+                                
+                                <h3
+                                    className='text-base md:text-xl font-medium select-text'
+                                >
+                                    akademitipmerkezi@windowslive.com
+                                </h3>
+                                
+                                </a>
+                        </div>
+
+
+                        <div
+                            className='text-left'
+                        >
+                            <a href="https://twitter.com/akademitip"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className='flex items-center gap-4 hover:text-secondary transition duration-300'
+                            >
+                                
+                                <FaXTwitter
+                                    className='text-3xl md:text-4xl shrink-0'
+                                />
+
+                                <h3
+                                    className='text-base md:text-xl font-medium select-text'
+                                >       
+                                    @akademitip
+                                </h3>
+                                
+                            </a>
+                        </div>                        
+                    </div>
+                </div>                
+            </div>
         </div>
     );
 };
